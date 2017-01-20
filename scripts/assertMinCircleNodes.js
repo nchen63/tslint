7f65315a7dd02aaee11d21998ec53e6e91ce3b45
Noah Chen <nchen@palantir.com>
2017-01-20 01:30:13 -0500

    fix

--- scripts/assertMinCircleNodes.js
+++ scripts/assertMinCircleNodes.js
@@ -1,6 +1,6 @@
 var requiredNodes = 4;
 var nodes = parseInt(process.argv[2], 10);
-if (requiredNodes != null && requiredNodes < nodes) {
+if (requiredNodes != null && requiredNodes > nodes) {
     console.error("ERROR: You must run CircleCI with 4 parallel nodes");
     console.error("    This ensures that different environments are tested for TSLint compatibility");
     console.error("    Go to https://circleci.com/gh/<YOUR ACCOUNT>/tslint/edit#parallel-builds");
